f35f04e413699629882bb69ae7be6fd90e4a1e04
flxxyz <i@mdzz.name>
2017-08-07 00:25:30 +0800

    complete 80%

--- application/controllers/Bing.php
+++ application/controllers/Bing.php
@@ -4,32 +4,35 @@ class Bing extends CI_Controller
 {
     public $id, $html;
     protected $url;
+    protected $data;
 
     public function __construct()
     {
         parent::__construct();
-        $this->id = 1;
-        $this->url = 'http://cn.bing.com/HPImageArchive.aspx?n=1&idx=' . $this->id;
     }
 
     public function index()
     {
-
+        //var_dump($this->uri->uri_to_assoc());
     }
 
-    public function api($type = 'url', $encode = 'json')
+    public function api()
     {
+        $type = get('type') ? get('type') : 'url';
+        $encode = get('encode') ? get('encode') : 'json';
+        $this->id = get('day') ? get('day') : 1;
+        $this->url = 'http://cn.bing.com/HPImageArchive.aspx?n=1&idx=' . $this->id;
         self::init($type, $encode);
     }
 
-    protected function init($type = 'url', $encode = 'json')
+    protected function init($type, $encode)
     {
-        $data = self::main();
-        //var_dump($data);
-        if ( $type == 'bg' ? true : false ) {
-            self::showImage($data);
+        $this->data = self::main();
+
+        if ( $type == 'bg' ) {
+            self::showImage();
         } else {
-            $this->dataStructure($data, $encode);
+            $this->dataStructure($this->data, $encode);
         }
     }
 
@@ -45,25 +48,40 @@ class Bing extends CI_Controller
         ];
     }
 
-    protected function showImage($data)
+    protected function showImage()
     {
+        $imgUrl = $this->data['imageurl'];
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $imgUrl);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_REFERER, $imgUrl);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36');
+        ob_start();
+        $return_content = ob_get_contents();
+        $code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        $result = curl_exec($ch);
+        ob_end_clean();
+        curl_close($ch);
+        if ( !( $code != '404' && $result ) )
+            return false;
+
         header('Content-Type: image/jpeg');
         @ob_end_clean();
-        echo 'image/jpeg';
-        echo getNetData($data['imageurl']);
+        echo $result;
         @ob_flush();
         @flush();
-        exit();
     }
 
     protected function handleHtmlDate($re)
     {
-        $url = '';
-        $html = $this->html;
+        $itemTxt = '';
+        if ( preg_match($re, $this->html, $matches) )
+            $itemTxt = $matches[1];
 
-        if ( preg_match($re, $html, $matches) )
-            $url = $matches[1];
-
-        return $url;
+        return $itemTxt;
     }
 }
